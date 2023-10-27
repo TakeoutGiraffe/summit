1864471d66be01200ba20bb5d80550e6f33f47f3
TakeoutGiraffe <149199152+TakeoutGiraffe@users.noreply.github.com>
2023-10-27 20:50:39 +0100

    .

--- summit.py
+++ summit.py
@@ -1,56 +1,15 @@
-import flet
-from flet import (
-    Container,
-    Icon,
-    Page,
-    Text,
-    AppBar,
-    PopupMenuButton,
-    PopupMenuItem,
-    colors,
-    icons,
-    margin
-)
+import flet as ft
+ 
+class Workspace(ft.UserControl)
+    def build(self):
+        return Text("Hello")
 
-from layout import AppLayout
- 
-class SummitApp:
-    def __init__(self, page: Page):
-        self.page = page
-        self.appbar_items = [
-            PopupMenuItem(text="Login"),
-            PopupMenuItem(),  # divider
-            PopupMenuItem(text="Settings")
-        ]
-        self.appbar = AppBar(
-            leading=Icon(icons.GRID_GOLDENRATIO_ROUNDED),
-            leading_width=100,
-            title=Text("Summit",size=32, text_align="start"),
-            center_title=False,
-            toolbar_height=75,
-            bgcolor=colors.LIGHT_BLUE_ACCENT_700,
-            actions=[
-                Container(
-                    content=PopupMenuButton(
-                        items=self.appbar_items
-                    ),
-                    margin=margin.only(left=50, right=25)
-                )
-            ],
-        )
-        self.page.controls.add(AppLayout())
-        self.page.appbar = self.appbar
-        self.page.update()
-
-if __name__ == "__main__":
- 
-    def main(page: Page):
- 
-        page.title = "Summit"
-        page.padding = 0
-        page.bgcolor = colors.BLUE_GREY_200
-        app = SummitApp(page)
-        page.add(app)
-        page.update()
+def main(page: Page):
+    page.title = "Summit"
+    page.padding = 0
+    page.bgcolor = colors.BLUE_GREY_200
+    workspace = Workspace()
+    page.add(workspace)
+    page.update()
  
     flet.app(target=main)
