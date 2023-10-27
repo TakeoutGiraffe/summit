f9ee7cf61df5eb79fe156b23dbc210ed66a820ba
TakeoutGiraffe <149199152+TakeoutGiraffe@users.noreply.github.com>
2023-10-27 21:06:37 +0100

    .

--- summit.py
+++ summit.py
@@ -3,8 +3,35 @@ from welcome import Welcome
  
 class Workspace(ft.UserControl):
     def build(self):
+
+        rail = ft.NavigationRail(
+            selected_index=0,
+            label_type=ft.NavigationRailLabelType.ALL,
+            # extended=True,
+            min_width=100,
+            min_extended_width=400,
+            leading=ft.FloatingActionButton(icon=ft.icons.CREATE, text="Add"),
+            group_alignment=-0.9,
+            destinations=[
+                ft.NavigationRailDestination(
+                    icon=ft.icons.FAVORITE_BORDER, selected_icon=ft.icons.FAVORITE, label="First"
+                ),
+                ft.NavigationRailDestination(
+                    icon_content=ft.Icon(ft.icons.BOOKMARK_BORDER),
+                    selected_icon_content=ft.Icon(ft.icons.BOOKMARK),
+                    label="Second",
+                ),
+                ft.NavigationRailDestination(
+                    icon=ft.icons.SETTINGS_OUTLINED,
+                    selected_icon_content=ft.Icon(ft.icons.SETTINGS),
+                    label_content=ft.Text("Settings"),
+                ),
+            ],
+            on_change=lambda e: print("Selected destination:", e.control.selected_index),
+        )
+
         return ft.Row([
-            ft.Text("SideBar"),
+            rail,
             Welcome()
         ])
 
