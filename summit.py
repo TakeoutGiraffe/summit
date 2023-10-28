bfb4d1b144b5fef8ff7132a016d6facc04edb554
TakeoutGiraffe <149199152+TakeoutGiraffe@users.noreply.github.com>
2023-10-28 21:09:38 +0100

    .

--- summit.py
+++ summit.py
@@ -12,6 +12,14 @@ class Workspace(ft.UserControl):
 
     def __init__(self):
         super().__init__()
+        self.welcome=Welcome()
+        self.apiendpoints=APIEndpoints()
+        self.schedules=Schedules()
+        self.scripts=Scripts()
+        self.security=Security()
+        self.tasks=Tasks()
+        self.settings=Settings()
+        self.dashboards=Dashboards()
         self.selectedControl=Welcome()
 
     def build(self):
@@ -19,25 +27,25 @@ class Workspace(ft.UserControl):
         def newScreenSelected(e):
             print (e.control.selected_index)
             if e.control.selected_index == 0:
-                self.selectedControl = APIEndpoints()
+                self.selectedControl = self.apiendpoints
                 self.update()
             elif e.control.selected_index == 1:
-                self.selectedControl = Scripts()
+                self.selectedControl = self.scripts
                 self.update()
             elif e.control.selected_index == 2:
-                self.selectedControl = Tasks()
+                self.selectedControl = self.security
                 self.update()
             elif e.control.selected_index == 3:
-                self.selectedControl = Schedules()
+                self.selectedControl = self.schedules
                 self.update()
             elif e.control.selected_index == 4:
-                self.selectedControl = Dashboards()
+                self.selectedControl = self.dashboards
                 self.update()
             elif e.control.selected_index == 5:
-                self.selectedControl = Security()
+                self.selectedControl = self.security
                 self.update()
             elif e.control.selected_index == 6:
-                self.selectedControl = Settings()
+                self.selectedControl = self.settings
                 self.update()
 
         rail = ft.NavigationRail(
