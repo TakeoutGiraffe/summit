181e4c2208cfe0e329c6c4578a17f5a8847f211e
TakeoutGiraffe <149199152+TakeoutGiraffe@users.noreply.github.com>
2023-10-29 16:54:23 +0000

    .

--- scripts.py
+++ scripts.py
@@ -4,7 +4,19 @@ import dbconnect as db
 def get_scripts_view():
     header = ft.Container(ft.Text("Script"))
     scripts = db.get_scripts()
-    lv = ft.ListView()
+    table = ft.DataTable()
+    table.columns = [
+        ft.DataColumn(ft.Text('ID')),
+        ft.DataColumn(ft.Text('Script Name')),
+        ft.DataColumn(ft.Text('Tools'))
+    ]
     for script in scripts:
-        lv.controls.append(ft.Text(script[1]))
-    return ft.Column([header,lv,],expand=True)
+        ft.rows.add(ft.DataRow(
+            cells=[
+                ft.DataCell(ft.Text(script[0])),
+                ft.DataCell(ft.Text(script[0])),
+                ft.DataCell(ft.Text(""))
+
+            ]
+        ))
+    return ft.Column([header,table],expand=True)
