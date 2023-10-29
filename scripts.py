54b27d9516bfba2673ffae680a2ea02513ec0091
TakeoutGiraffe <149199152+TakeoutGiraffe@users.noreply.github.com>
2023-10-29 15:36:20 +0000

    .

--- scripts.py
+++ scripts.py
@@ -3,12 +3,13 @@ import dbconnect as db
 
 class Scripts(ft.UserControl):
 
-    def __init__(self):
+    def __init__(self,height):
         super().__init__(self)
         self.scripts = db.get_scripts()
+        #self.height=height
 
     def build(self):
-        lv = ft.ListView(expand=true)
+        lv = ft.ListView(height=700)
         for script in self.scripts:
             lv.controls.append(ft.Text(script[1]))
         return lv
