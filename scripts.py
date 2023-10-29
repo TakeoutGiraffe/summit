b5c7c50405978ff7c18b5fce9905a95d43193dd3
TakeoutGiraffe <149199152+TakeoutGiraffe@users.noreply.github.com>
2023-10-29 16:12:17 +0000

    .

--- scripts.py
+++ scripts.py
@@ -6,10 +6,10 @@ class Scripts(ft.UserControl):
     def __init__(self):
         super().__init__(self)
         self.scripts = db.get_scripts()
-        #self.height=height
+        self.height=700
 
     def build(self):
         #lv = ft.ListView()
        # for script in self.scripts:
         #    lv.controls.append(ft.Text(script[1]))
-        return ft.Column([Text("Scripts"),Text("Hello"),])
+        return ft.Column([Text("Scripts"),Text("Hello"),],expand=True)
