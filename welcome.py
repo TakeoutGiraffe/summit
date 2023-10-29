d51b3d1f3cbd5b1fb0b3a4543e38577b8ba98b45
TakeoutGiraffe <149199152+TakeoutGiraffe@users.noreply.github.com>
2023-10-29 19:21:18 +0000

    .

--- welcome.py
+++ welcome.py
@@ -3,5 +3,5 @@ import flet as ft
 class WelcomeView(ft.Column):
     def __init__(self):
         super().__init__()
-        controls=[ft.Text("Welcome")]
-        expand=True
+        self.controls=[ft.Text("Welcome")]
+        self.expand=True
