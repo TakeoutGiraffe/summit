dc148ac8d1994a1e71292f25e43d74e970fdcb80
TakeoutGiraffe <149199152+TakeoutGiraffe@users.noreply.github.com>
2023-10-29 21:55:44 +0000

    .

--- scripteditor.py
+++ scripteditor.py
@@ -5,7 +5,14 @@ class ScriptEditorView(ft.Column):
 
     def __init__(self,script_id):
         super().__init__()
-        script=db.get_script_with_id(script_id)[0]
+
+        header = ft.Text(script[1], size=36)
+
+        script=db.get_script_with_id(script_id)
+        script_text = ft.TextField(
+            value=script[2]
+            multiline=True
+        )
 
         footer = ft.Row([ft.ElevatedButton("New Script", on_click=self.new_script_clicked)  ])
         self.controls=[header, script_text, footer]
