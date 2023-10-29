d2ae33297c0a00d9a9c0a250a48b175bda43383b
TakeoutGiraffe <149199152+TakeoutGiraffe@users.noreply.github.com>
2023-10-29 19:54:36 +0000

    .

--- tasks.py
+++ tasks.py
@@ -3,4 +3,4 @@ import flet as ft
 class TasksView(ft.Column):
     def __init__(self):
         super().__init__()
-        self.controls= ft.Text("Tasks")
+        self.controls= [ft.Text("Tasks")]
