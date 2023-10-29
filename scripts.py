39bb2c896007e671ddc882f7f1151934f5f48fff
TakeoutGiraffe <149199152+TakeoutGiraffe@users.noreply.github.com>
2023-10-29 16:31:56 +0000

    .

--- scripts.py
+++ scripts.py
@@ -2,4 +2,4 @@ import flet as ft
 import dbconnect as db
 
 def get_scripts_view():
-    return ft.Column([Text("Scripts"),Text("Hello"),],expand=True)
+    return ft.Column([ft.Text("Scripts"),ft.Text("Hello"),],expand=True)
