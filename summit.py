de463edb86609145c798f47180138f816e475ab9
TakeoutGiraffe <149199152+TakeoutGiraffe@users.noreply.github.com>
2023-10-31 19:55:43 +0000

    .

--- summit.py
+++ summit.py
@@ -37,7 +37,7 @@ class Application(ft.Row):
     def __init__(self,page:ft.page):
         super().__init__()
         self.page=page
-        if 'userlogin' in page.session
+        if 'userlogin' in page.session:
             rail = ft.NavigationRail(
                     selected_index=0,
                     label_type=ft.NavigationRailLabelType.ALL,
