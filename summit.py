394cff4257fb32a09a3dfb9ea6b343eeebefd552
TakeoutGiraffe <149199152+TakeoutGiraffe@users.noreply.github.com>
2023-10-31 19:43:17 +0000

    .

--- summit.py
+++ summit.py
@@ -37,63 +37,71 @@ class Application(ft.Row):
     def __init__(self,page:ft.page):
         super().__init__()
         self.page=page
-        rail = ft.NavigationRail(
-                selected_index=0,
-                label_type=ft.NavigationRailLabelType.ALL,
-                min_width=100,
-                min_extended_width=300,
-                extended=True,
-                group_alignment=-0.9,
-                destinations=[
-                    ft.NavigationRailDestination(
-                        icon=ft.icons.FAVORITE_BORDER, selected_icon=ft.icons.FAVORITE, label="API Endpoints"
-                    ),
-                    ft.NavigationRailDestination(
-                        icon=ft.icons.FAVORITE_BORDER, selected_icon=ft.icons.FAVORITE, label="Scripts"
-                    ),
-                    ft.NavigationRailDestination(
-                        icon=ft.icons.FAVORITE_BORDER, selected_icon=ft.icons.FAVORITE, label="Running Tasks"
-                    ),
-                    ft.NavigationRailDestination(
-                        icon=ft.icons.FAVORITE_BORDER, selected_icon=ft.icons.FAVORITE, label="Schedules"
-                    ),
-                    ft.NavigationRailDestination(
-                        icon=ft.icons.FAVORITE_BORDER, selected_icon=ft.icons.FAVORITE, label="Dashboards"
-                    ),
-                    ft.NavigationRailDestination(
-                        icon=ft.icons.FAVORITE_BORDER, selected_icon=ft.icons.FAVORITE, label="Security"
-                    ),
-                    ft.NavigationRailDestination(
-                        icon=ft.icons.FAVORITE_BORDER, selected_icon=ft.icons.FAVORITE, label="Settings"
+        if page.session['userid'] == Nothing:
+            self.height=700
+            self.controls=[
+                ft.TextField("Login"),
+                ft.TextField("Password"),
+                ft.Button()"Login")
+            ]
+        else:
+            rail = ft.NavigationRail(
+                    selected_index=0,
+                    label_type=ft.NavigationRailLabelType.ALL,
+                    min_width=100,
+                    min_extended_width=300,
+                    extended=True,
+                    group_alignment=-0.9,
+                    destinations=[
+                        ft.NavigationRailDestination(
+                            icon=ft.icons.FAVORITE_BORDER, selected_icon=ft.icons.FAVORITE, label="API Endpoints"
+                        ),
+                        ft.NavigationRailDestination(
+                            icon=ft.icons.FAVORITE_BORDER, selected_icon=ft.icons.FAVORITE, label="Scripts"
+                        ),
+                        ft.NavigationRailDestination(
+                            icon=ft.icons.FAVORITE_BORDER, selected_icon=ft.icons.FAVORITE, label="Running Tasks"
+                        ),
+                        ft.NavigationRailDestination(
+                            icon=ft.icons.FAVORITE_BORDER, selected_icon=ft.icons.FAVORITE, label="Schedules"
+                        ),
+                        ft.NavigationRailDestination(
+                            icon=ft.icons.FAVORITE_BORDER, selected_icon=ft.icons.FAVORITE, label="Dashboards"
+                        ),
+                        ft.NavigationRailDestination(
+                            icon=ft.icons.FAVORITE_BORDER, selected_icon=ft.icons.FAVORITE, label="Security"
+                        ),
+                        ft.NavigationRailDestination(
+                            icon=ft.icons.FAVORITE_BORDER, selected_icon=ft.icons.FAVORITE, label="Settings"
+                        ),
+                    ],
+                    on_change=self.new_screen_selected,
+                    )
+
+            self.page.appbar = ft.AppBar(
+                leading=ft.Icon(ft.icons.PALETTE),
+                leading_width=40,
+                title=ft.Text("AppBar Example"),
+                center_title=False,
+                bgcolor=ft.colors.SURFACE_VARIANT,
+                actions=[
+                    ft.IconButton(ft.icons.WB_SUNNY_OUTLINED),
+                    ft.IconButton(ft.icons.FILTER_3),
+                    ft.PopupMenuButton(
+                        items=[
+                            ft.PopupMenuItem(text="Item 1"),
+                            ft.PopupMenuItem(),  # divider
+                        ]
                     ),
                 ],
-                on_change=self.new_screen_selected,
-                )
+            )
 
-        self.page.appbar = ft.AppBar(
-            leading=ft.Icon(ft.icons.PALETTE),
-            leading_width=40,
-            title=ft.Text("AppBar Example"),
-            center_title=False,
-            bgcolor=ft.colors.SURFACE_VARIANT,
-            actions=[
-                ft.IconButton(ft.icons.WB_SUNNY_OUTLINED),
-                ft.IconButton(ft.icons.FILTER_3),
-                ft.PopupMenuButton(
-                    items=[
-                        ft.PopupMenuItem(text="Item 1"),
-                        ft.PopupMenuItem(),  # divider
-                    ]
-                ),
-            ],
-        )
-
-        self.height=700
-        self.controls=[
-            rail,
-            ft.VerticalDivider(),
-            WelcomeView(),
-            ]
+            self.height=700
+            self.controls=[
+                rail,
+                ft.VerticalDivider(),
+                WelcomeView(),
+                ]
 
     
 
@@ -102,15 +110,9 @@ class Application(ft.Row):
 def main(page: ft.page):
     page.title = "Summit"
     page.padding = 0
-    if user:
-        app=Application(page)
-        page.add(app)
-        page.update()
-    else:
-        page.add(ft.Column([
-            ft.TextField("User Name"),
-            ft.TextField("Password")
-        ]))
-        page.update
+    page.add(UserLogin)
+    app=Application(user)
+    page.add(app)
+    page.update()
 
 ft.app(target=main)
