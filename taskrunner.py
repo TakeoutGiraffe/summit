f093b7c406edf1c150b7f6bd192eee45ace05cf3
TakeoutGiraffe <149199152+TakeoutGiraffe@users.noreply.github.com>
2023-11-11 17:45:57 +0000

    .

--- taskrunner.py
+++ taskrunner.py
@@ -1,4 +1,8 @@
+import dbconnect as db
 class TaskRunner:
     
-    def __init__(self):
-        self.test = 1
+    def __init__(self,task):
+        self.task = task
+        
+    def run(self):
+        db.set_task_status(self.task, 1)
